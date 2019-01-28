fcd21fde52ae2765504552061bd75b8b474583fa
Katsukiniwa <katsukiniwa0217@gmail.com>
2019-01-28 14:16:06 +0900

    last

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,13 +16,13 @@ class PostsController < ApplicationController
     @post = Post.new(posts_params)
     if @post.save
       data = [["binary_file", @post.post_picture, {filename: "binary_file_path.bin"}]]
-           url = URI.parse("https://gateway.watsonplatform.net/visual-recognition/api/v3/classify?&version=2018-03-19")
-           req = Net::HTTP::Post.new(url.path)
-           req.set_form(data, "multipart/form-data")
-           res = Net::HTTP.start(url.host, url.port) {|http|
-             http.request(req)
-           }
-           puts res.body
+      url = URI.parse("https://gateway.watsonplatform.net/visual-recognition/api/v3/classify?&version=2018-03-19")
+      req = Net::HTTP::Post.new(url.path)
+      req.set_form(data, "multipart/form-data")
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.request(req)
+      }
+      puts res.body
       redirect_to @post, notice: "作品を投稿しました"
     else
       render "new"
